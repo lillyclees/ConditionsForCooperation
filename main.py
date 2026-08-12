c9d0abdaed1d55fb2055bac3d7747977cd80d722
lillyclees <gale.anime.otaku@gmail.com>
2026-08-12 18:58:25 -0400

    updated play loop to account for number of episodes and number of epochs.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from agent import *
 def create_agents(pop_size):
     players = []
     for i in range(pop_size):
-        player = Agent(pop_size, x, y)
-        player.Random_starting_probs()
+        player = Agent(pop_size, x, y, dec_rule="S", risk_av=False)
+        player.random_starting_probs()
         players.append(player)
 
     return players
@@ -16,62 +16,74 @@ def plot_outcome(data):
     plt.show()
 
 
-fields = ['Episode', 'Acceptances', 'Beliefs', 'Total Payoffs']
+fields = ['Epoch', 'Acceptances', 'Beliefs', 'Total Payoffs']
 game_type = "simple"
 game_no = "1"
-start_probs = "1n_strats"
-file_name = game_type + game_no + start_probs
+start_probs = "random"
 
-avg_probs = []
 pop_size = 100
 x = 5
 y = 10
-n_episodes = 10
-
-players = create_agents(pop_size)
-
-data = []
-
-for episode in range(n_episodes):
-    accept = False
-    acceptances = 0
-    prob_accepts = []
-    total_payoffs = []
-
-    # make moves
-    for player in players:
-        if player.move() == "accept":
-            accept = True
-            acceptances += 1
-        prob_accepts.append(player.prob_accept)
-
-    # disperse payoffs and update beliefs
-    for player in players:
-        player.payoff(accept)
-        player.update_belief(acceptances)
-        total_payoffs.append(player.total_util)
-
-    #players[0].plot_dist()
-
-    episode_data = {}
-    episode_data["Episode"] = episode
-    episode_data["Acceptances"] = acceptances
-    episode_data["Prob Accepts"] = prob_accepts
-    episode_data["Total Payoffs"] = total_payoffs
-    data.append(episode_data)
-
-    avg_probs.append(sum(prob_accepts) / pop_size)
-
-plot_outcome(avg_probs)
+n_episodes = 1
+episode_length = 1000
 
 
-with open(f'{file_name}.csv', 'w', newline='') as f:
-    writer = csv.DictWriter(f, fieldnames=["Episode", "Acceptances", "Prob Accepts", "Total Payoffs"])
-    writer.writeheader()
-    writer.writerows(data)
+def run_episodes(n_episodes, episode_length, x, y, pop_size, game_no, game_type, start_probs):
+    for episode in range(n_episodes):
+        avg_probs = run_episode(episode_length, x, y, pop_size, game_no + str(episode), game_type, start_probs)
+        plot_outcome(avg_probs)
+
+def run_episode(episode_length, x, y, pop_size, game_no, game_type, start_probs):
+    file_name = game_type + game_no + start_probs + "x=" + str(x) + "y=" + str(y)
+    players = create_agents(pop_size)
+
+    avg_probs = []
+    data = []
+
+    for epoch in range(episode_length):
+        print(epoch)
+        accept = False
+        acceptances = 0
+        prob_accepts = []
+        total_payoffs = []
+
+        # make moves
+        for player in players:
+            if player.move() == "accept":
+                accept = True
+                acceptances += 1
+            if player.prob_accept > 1:
+                print("wrong")
+            prob_accepts.append(player.prob_accept)
+
+        # disperse payoffs and update beliefs
+        for player in players:
+            player.payoff(accept)
+            player.update_belief(acceptances)
+            total_payoffs.append(player.total_util)
+
+        episode_data = {}
+        episode_data["Epoch"] = epoch
+        episode_data["Acceptances"] = acceptances
+        episode_data["Prob Accepts"] = prob_accepts
+        episode_data["Total Payoffs"] = total_payoffs
+        data.append(episode_data)
 
 
+        avg_probs.append(sum(prob_accepts) / pop_size)
 
+
+        #players[0].plot_dist()
+
+
+    with open(f'{file_name}.csv', 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=["Epoch", "Acceptances", "Prob Accepts", "Total Payoffs"])
+        writer.writeheader()
+        writer.writerows(data)
+
+    return avg_probs
+
+run_episodes(n_episodes, episode_length, x, y, pop_size, game_no, game_type, start_probs)
 
 
 
